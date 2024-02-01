c1f53ad31c6d26ed6860627256c7345936f185dd
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-01 09:56:06 -0600

    Println

---
+++ week04/println.kt
@@ -0,0 +1,10 @@
+// HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+// TODO
+
+fun main(){
+ println("Hello world!")
+    println("My name is Lydia")
+    println("I'm taking lesson ITSE1329")
+}
