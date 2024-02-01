27ebd9b48c980468557513d413a5d60d5b8b891c
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-01 10:46:39 -0600

    names

---
+++ week04/input.kt
@@ -0,0 +1,12 @@
+// VarAndVal/Task1.kt
+package varAndValExercise1
+
+fun main() {
+  // TODO
+
+    val first = "Lydia"
+    val last = "Htut"
+    var age = 16
+    println ("Hello world my name is $first $last, and I am $age year old")
+    println ("Did I tell you that my firts name is $first")
+}
