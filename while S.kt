106cc16bbd1ae39bf077773d3abd9d6a20cd923c
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-06 10:49:44 -0600

    Create whileSample.kt

---
+++ while S.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var n = 10
+    val d = 3
+    print(n)
+    while(n > d) {
+        n -= d
+        print(" - $d")
+    }
+            println(" = $n")
+    
+            var n = 10
+            print(n)
+            n %= d
+            println("% $d = $n")
+        }
+    
+    
