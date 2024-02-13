a6b110ac29139a94c885941d93b6f92fa4a18096
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-13 10:39:47 -0600

    starpattern

---
+++ unit02/starpattern.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+}
