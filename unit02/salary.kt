bbc1cc727d288693bec6460c0b1961942cd68cc2
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-13 11:01:11 -0600

    salary

---
+++ unit02/salary.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var name: String
+    name = "Lydia"
+    val age: Int = 16
+    val annualPay: Double = 50000.0
+
+    println("my name is $name, my age is $age and")
+    println("I hope to earn $$annualPay per year")
+}
