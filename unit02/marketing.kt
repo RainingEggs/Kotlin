0d4ffdf7faaec3afda3e478d080514bc1c36de81
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-15 10:12:53 -0600

    marketing

--- unit02/marketing.kt
+++ unit02/marketing.kt
@@ -1,9 +1,9 @@
 fun main() {
     //made by Lydia Htut 2/15/2024
     
-    val friendsCount = 600 
+    val friendsCount = 500 
 
-    if (friendsCount > 500) {
+    if (friendsCount >= 500) {
         println("Adopt a Dog Today!")
     } else {
         println("Buy Cat Food At 20% Off")
