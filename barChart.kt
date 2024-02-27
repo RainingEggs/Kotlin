17b58f66d904c7674075d856dd147b024b31f840
RainingEggs <157508010+RainingEggs@users.noreply.github.com>
2024-02-27 10:51:56 -0600

    bar

--- barChart.kt
+++ barChart.kt
@@ -1,19 +1,20 @@
 fun main() {
     val sales = IntArray(5)
 
-    // Input sales for each store
-    for (i in 0 until 5) {
-        print("Enter today's sales for store ${i + 1}: ")
-        sales[i] = readLine()?.toIntOrNull() ?: 0
-    }
+    
+    sales[0] = 1000
+    sales[1] = 1200
+    sales[2] = 1800
+    sales[3] = 800
+    sales[4] = 1900
 
     // Display sales bar chart
-    println("\nSALES BAR CHART")
+    println("SALES BAR CHART")
     for (i in 0 until 5) {
         print("Store ${i + 1}: ")
         repeat(sales[i] / 100) {
             print("*")
         }
-        println() // Move to the next line after printing each store's bar
+        println() 
     }
 }
